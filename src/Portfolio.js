95562cfefffa26c572ae5ec94ec5bf2beff232fc
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-12-09 17:59:24 -0600

    added getstrong to portfolio page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,5 +1,5 @@
 import './App.css';
-import seth from './seth.JPG'
+import getstrong from './getstrong.png'
 
 function Portfolio() {
     return (<>
@@ -7,6 +7,21 @@ function Portfolio() {
             <h1>PORTFOLIO</h1>
             <h3>Here are some projects I've done
             </h3>
+        </div>
+
+        <div className='projects-list'>
+            <div className='project'>
+                <h3>GetStrong</h3>
+                <img src={getstrong} height={250}></img>
+                <p>Having trouble keeping track of your workouts?
+                    GetStrong has you covered! This React application
+                    simplifies logging your training sessions and even
+                    has a built in weight calculator!
+                </p>
+                <a href='https://github.com/slandonlewis/getStrong'>
+                    <button>VIEW ON GITHUB</button>
+                </a>
+            </div>
         </div>
     </>
     );
