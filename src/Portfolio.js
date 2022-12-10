15ecf833e92962e74843b4d7e00c432135cf7968
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-12-09 18:22:01 -0600

    stylized project list

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -22,6 +22,19 @@ function Portfolio() {
                     <button>VIEW ON GITHUB</button>
                 </a>
             </div>
+
+            <div className='project'>
+                <h2>GetStrong</h2>
+                <img className='thumbnail' src={getstrong} height={300} width={450}></img>
+                <p>Having trouble keeping track of your workouts?<br></br>
+                    GetStrong has you covered! This React application
+                    simplifies logging your training sessions and even
+                    has a built in weight calculator!
+                </p>
+                <a href='https://github.com/slandonlewis/getStrong'>
+                    <button>VIEW ON GITHUB</button>
+                </a>
+            </div>
         </div>
     </>
     );
