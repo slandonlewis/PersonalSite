649985b71e7b8f0197f75716f0dd940285fcbd0e
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-12-09 19:31:24 -0600

    added link to interview

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import './App.css';
-import seth from './seth.JPG'
+import ad from './ad.png'
+import spotify from './spotify.png'
 
 function Home() {
   return (<>
@@ -9,6 +10,15 @@ function Home() {
         fullstack software developer from Elmwood,
         TN.
       </h3>
+    </div>
+
+    <div className='spotify'>
+      <img src={ad} height={325}></img>
+      <a href='https://open.spotify.com/show/4HlYtLKw0KD8IEpWJQty9R' target='blank'>
+        <button>
+          <img src={spotify} height={120}></img>
+        </button>
+      </a>
     </div>
   </>
   );
