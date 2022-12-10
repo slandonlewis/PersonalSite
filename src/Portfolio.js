57ef319d273a70bfe0fca8b8e4a359885154c1b5
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-12-09 18:37:28 -0600

    added geekgallery to projects list

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,5 +1,6 @@
 import './App.css';
 import getstrong from './getstrong.png'
+import geekgallery from './geekgallery.png'
 
 function Portfolio() {
     return (<>
@@ -18,20 +19,21 @@ function Portfolio() {
                     simplifies logging your training sessions and even
                     has a built in weight calculator!
                 </p>
-                <a href='https://github.com/slandonlewis/getStrong'>
+                <a href='https://github.com/slandonlewis/getStrong'
+                    target='blank'>
                     <button>VIEW ON GITHUB</button>
                 </a>
             </div>
 
             <div className='project'>
-                <h2>GetStrong</h2>
-                <img className='thumbnail' src={getstrong} height={300} width={450}></img>
-                <p>Having trouble keeping track of your workouts?<br></br>
-                    GetStrong has you covered! This React application
-                    simplifies logging your training sessions and even
-                    has a built in weight calculator!
+                <h2>GeekGallery</h2>
+                <img className='thumbnail' src={geekgallery} height={300} width={450}></img>
+                <p>Home of many inspired artists who wish to share
+                    their talents, as well as their passions for anime,
+                    manga, comics, movies, and all things geeky!
                 </p>
-                <a href='https://github.com/slandonlewis/getStrong'>
+                <a href='https://github.com/slandonlewis/GeekGallery'
+                    target='blank'>
                     <button>VIEW ON GITHUB</button>
                 </a>
             </div>
