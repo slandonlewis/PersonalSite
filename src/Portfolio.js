69e531542f563593a30cea41b6754890aecf26fb
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-12-09 18:18:10 -0600

    styled project cards

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,15 +5,15 @@ function Portfolio() {
     return (<>
         <div className='home'>
             <h1>PORTFOLIO</h1>
-            <h3>Here are some projects I've done
+            <h3>Notable Projects
             </h3>
         </div>
 
         <div className='projects-list'>
             <div className='project'>
-                <h3>GetStrong</h3>
-                <img src={getstrong} height={250}></img>
-                <p>Having trouble keeping track of your workouts?
+                <h2>GetStrong</h2>
+                <img className='thumbnail' src={getstrong} height={300} width={450}></img>
+                <p>Having trouble keeping track of your workouts?<br></br>
                     GetStrong has you covered! This React application
                     simplifies logging your training sessions and even
                     has a built in weight calculator!
